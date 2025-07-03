c8a581193688b9fe00b4149ba6ac2e22d31bc8ec
saronmeles4 <saronmeles4@gmail.com>
2025-07-03 14:57:12 +0300

    SQL queries are included inside the python script

--- MySQLServer.py
+++ MySQLServer.py
@@ -8,13 +8,55 @@ mydb = mysql.connector.connect(
 )
 cursor = mydb.cursor()
 
-with open("alx_book_store.sql", "r")as file:
-    sql_script = file.read()
+
+sql_script="""
+
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+CREATE TABLE Authors(
+author_id INT PRIMARY KEY,
+author_name VARCHAR(251)
+);
+
+CREATE TABLE Books(
+book_id INT PRIMARY KEY,
+title VARCHAR(130),
+author_id INT,
+price DOUBLE,
+publication_date DATE,
+FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE Customers(
+ customer_id INT PRIMARY KEY,
+ customer_name VARCHAR(215),
+ email VARCHAR(215),
+ address TEXT
+);
+
+CREATE TABLE Orders (
+order_id INT PRIMARY KEY,
+customer_id INT,
+order_date DATE,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details(
+orderdetailid INT PRIMARY KEY,
+quantity DOUBLE,
+order_id INT,
+book_id INT,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+"""
+
 statements = sql_script.split(';')
 try:
     for stmt in statements:
         stmt = stmt.strip()
-        if stmt:  # skip empty lines
+        if stmt: 
             cursor.execute(stmt)
     print("Database 'alx_book_store' created successfully!")
 except mysql.connector.Error as err:
